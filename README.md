2fdaad03f73e780ec5fae4c0c56d90c56df5e87f
Marcus J. Hildum <marcus.hildum@gmail.com>
2026-07-31 13:16:49 -0700

    Prepare 0.0.17 release

--- README.md
+++ README.md
@@ -69,7 +69,7 @@ update itself:
 
 ```sh
 arise sync
-arise -1 --reinstall =sys-apps/arise-0.0.16
+arise -1 --reinstall =sys-apps/arise-0.0.17
 ```
 
 Git repositories synchronize concurrently. Arise honors Portage-compatible
--- cmd/arise/main.go
+++ cmd/arise/main.go
@@ -25,7 +25,7 @@ import (
 )
 
 // version is replaced by release builds with -ldflags "-X main.version=...".
-var version = "0.0.16"
+var version = "0.0.17"
 var commandContext = context.Background()
 
 func versionLine() string {
